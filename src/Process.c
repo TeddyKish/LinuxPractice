832a6cb610c187af436c05ef99d32903020c8702
David Sariel <davidsa@openu.ac.il>
2020-05-04 10:00:38 -0400

    Finished exercise 2.0-recursive forks that create a binary tree of processes

--- src/Process.c
+++ src/Process.c
@@ -12,26 +12,50 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(void) {
+void error_exit(void)
+{
+	printf("ERROR");
+	exit(-1);
+}
 
+void recursive_double_fork(unsigned int recursive_level)
+{
+	if (recursive_level == 0)
+	{
+		return;
+	}
 
-	// Ex1
-//	pid_t pid;
-//
-//	if ((pid = fork()) == 0)
-//	{
-//		printf("CHILD: My pid is %ld, My parent is %ld \n", (long)getpid(), (long)getppid());
-//	}
-//	else
-//	{
-//		wait();
-//		printf("PARENT: My pid is %ld, My child is is %ld \n", (long)getpid(), (long) pid);
-//	}
-//
-//	return EXIT_SUCCESS;
-	// End EX1
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		error_exit();
+	}
+	else if (pid == 0)
+	{
+		recursive_double_fork(recursive_level - 1);
+	}
+	else if (pid != 0)
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			error_exit();
+		}
+		else if (pid == 0)
+		{
+			recursive_double_fork(recursive_level - 1);
+		}
+	}
 
+	printf("my pid is %ld \n", (long) getpid());
 
+	// currently it exits after the forks
+	exit(0);
+}
 
+int main(void)
+{
+	// Ex2.0
+	recursive_double_fork(4);
 
 }
