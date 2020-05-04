beab3de35e716739cff2f9a33be1f1d8b8963f42
David Sariel <davidsa@openu.ac.il>
2020-05-04 07:04:03 -0400

    Finished exercise 1- fork, pid

---
+++ src/Process.c
@@ -0,0 +1,37 @@
+/*
+ ============================================================================
+ Name        : Process.c
+ Author      : TeddyKish
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void) {
+
+
+	// Ex1
+//	pid_t pid;
+//
+//	if ((pid = fork()) == 0)
+//	{
+//		printf("CHILD: My pid is %ld, My parent is %ld \n", (long)getpid(), (long)getppid());
+//	}
+//	else
+//	{
+//		wait();
+//		printf("PARENT: My pid is %ld, My child is is %ld \n", (long)getpid(), (long) pid);
+//	}
+//
+//	return EXIT_SUCCESS;
+	// End EX1
+
+
+
+
+}
