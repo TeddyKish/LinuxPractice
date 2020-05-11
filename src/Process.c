da26ff9aafd4c204a7ab821465e7e2174a9521de
Teddy Kishinevsky <kishtedy@gmail.com>
2020-05-11 12:07:26 -0400

    Finished exercise 3- Printing pids and their corresponding commands

--- src/Process.c
+++ src/Process.c
@@ -11,20 +11,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <signal.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-#define CHILD_PROCESS_FORK_VALUE (0)
-#define SUCCESS_VALUE (0)
+#define BUFFER_SIZE (200)
 #define ERROR_VALUE (-1)
-
-static const unsigned int s_root_height = 4;
-static const unsigned int s_buffer_size = 100;
-
-pid_t root_pid = 0;
-pid_t right_child_pid = 0;
-pid_t left_child_pid = 0;
-
-int signal_sent[30];
 
 void error_exit(const char* msg)
 {
@@ -32,167 +26,69 @@ void error_exit(const char* msg)
 	exit(-1);
 }
 
-void death_signal_handler(int signo)
+int is_process_dirname(const char* entryname, unsigned char type)
 {
-	if (signo != SIGRTMIN)
+	if (type != DT_DIR)
 	{
-		error_exit("Death signal handler didn't receive SIGUSR1\n");
-
+		return 0;
 	}
 
-	// If the process isn't in the last level in the tree
-	if (left_child_pid != 0 && right_child_pid != 0)
+	while (*entryname != '\0')
 	{
-		union sigval empty_union;
-		sigqueue(left_child_pid, SIGRTMIN, empty_union);
-		sigqueue(right_child_pid, SIGRTMIN, empty_union);
+		if (!(isdigit(*entryname)))
+		{
+			return 0;
+		}
 
-		waitpid(left_child_pid, NULL, 0);
-		waitpid(right_child_pid, NULL, 0);
+		entryname++;
 	}
 
-	if (root_pid == getpid())
-	{
-		system("echo -n ps says: && ps -A | grep Process | wc -l");
-	}
-
-	printf("I am dead: %ld\n", (long)getpid());
-	exit (0);
+	return 1;
 }
 
-void general_signal_handler(int signo)
+void reset_buffer(char* buffer, unsigned int size)
 {
-	if (!(SIGRTMIN + 1 <= signo && signo <= SIGRTMAX))
+	for (int i = 0; i < size; i++)
 	{
-		error_exit("Unsupported signal\n");
+		buffer[i] = '\0';
 	}
-
-	signal_sent[signo - SIGRTMIN - 1] = 1;
-}
-
-void recursive_double_fork(unsigned int recursive_level, char* placement, int range_start, int range_end)
-{
-	int this_rtsig = range_end;
-	char* new_placement = (char*) malloc(s_buffer_size);
-	printf("%s \n", placement);
-
-	if (recursive_level != 0)
-	{
-		int left_child_range_end = (range_start + range_end - 1) / 2;
-
-		left_child_pid = fork();
-		if (left_child_pid == ERROR_VALUE)
-		{
-			error_exit("Can't fork left child\n");
-		}
-		else if (left_child_pid == CHILD_PROCESS_FORK_VALUE)
-		{
-			snprintf(new_placement, s_buffer_size, "%s->Left", placement);
-			recursive_double_fork(recursive_level - 1, new_placement, range_start, left_child_range_end);
-		}
-		else
-		{
-			right_child_pid = fork();
-			if (right_child_pid == ERROR_VALUE)
-			{
-				error_exit("Can't fork right child\n");
-			}
-			else if (right_child_pid == CHILD_PROCESS_FORK_VALUE)
-			{
-				snprintf(new_placement, s_buffer_size, "%s->Right", placement);
-				recursive_double_fork(recursive_level - 1, new_placement, left_child_range_end + 1, range_end - 1);
-			}
-		}
-	}
-	else
-	{
-		left_child_pid = 0;
-		right_child_pid = 0;
-	}
-
-	// If this is the root process
-	if (recursive_level == s_root_height)
-	{
-		printf("Root waiting for all the signals...\n");
-
-		// Verifies that a signal from every child process has been received.
-		int cond = 1;
-
-		while (cond)
-		{
-			cond = 0;
-
-			for (int i = 0; i < 30; i++)
-			{
-				if (signal_sent[i] == 0)
-				{
-					// If one of the signals wasn't received, continue iterating the while loop
-					cond = 1;
-					break;
-				}
-			}
-		}
-
-		printf("Root received all of the signals \n");
-
-		char pstree_command[s_buffer_size];
-		snprintf(pstree_command, s_buffer_size, "pstree %ld -c", (long)root_pid);
-		system(pstree_command);
-
-		system("echo -n ps says: && ps -A | grep Process | wc -l");
-		printf("Press any key to kill them all! \n");
-		getchar();
-
-		free(new_placement);
-		raise(SIGRTMIN);
-	}
-
-	sleep(5);
-
-	printf("PID( %ld ): waiting to die (SIGNO: %d) \n", (long)getpid(), SIGRTMIN + this_rtsig);
-	union sigval empty_union;
-	sigqueue(root_pid, SIGRTMIN + this_rtsig, empty_union);
-
-	free(new_placement);
-	pause();
-
 }
 
 int main(void)
 {
-	// Ex2.2
-	root_pid = getpid();
+	DIR* proc = opendir("/proc");
+	struct dirent* curr_entry = readdir(proc);
 
-	if ((SIGRTMAX - SIGRTMIN) < 30)
+	char comm_filename[BUFFER_SIZE] = "";
+	char comm_content[BUFFER_SIZE] = "";
+
+	while (curr_entry != NULL)
 	{
-		error_exit("Not enough RT-Signals");
-	}
-
-	// Root registering all of the signal functions
-	struct sigaction death_action;
-	death_action.sa_handler = death_signal_handler;
-	const struct sigaction* p_death_action = &death_action;
-
-	if (sigaction(SIGRTMIN, p_death_action, NULL) == ERROR_VALUE)
-	{
-		error_exit("Can't register DEATH signal handler");
-	}
-
-	struct sigaction sigac;
-	sigac.sa_flags = SA_RESTART | SA_RESETHAND;
-	sigac.sa_handler = general_signal_handler;
-	const struct sigaction* p_sigac = &sigac;
-
-	for (int i = SIGRTMIN + 1; i <= SIGRTMIN + 30; i++)
-	{
-		if (sigaction(i, p_sigac, NULL) == ERROR_VALUE)
+		if (is_process_dirname(curr_entry->d_name, curr_entry->d_type))
 		{
-			error_exit("Can't catch an RT signal\n");
+			if (snprintf(comm_filename, BUFFER_SIZE, "/proc/%s/comm", curr_entry->d_name) < 0)
+			{
+				error_exit("snprintf failed \n");
+			}
+
+			int cmdline_fd = open(comm_filename, O_RDONLY);
+
+			if (cmdline_fd == ERROR_VALUE)
+			{
+				error_exit("Cannot open comm file\n");
+			}
+
+			if (read(cmdline_fd, comm_content, BUFFER_SIZE - 1) == ERROR_VALUE)
+			{
+				error_exit("Couldn't read comm content\n");
+			}
+
+			printf("%s %s", curr_entry->d_name, comm_content);
+
+			reset_buffer(comm_filename, BUFFER_SIZE);
+			reset_buffer(comm_content, BUFFER_SIZE);
 		}
 
-		signal_sent[i - SIGRTMIN - 1] = 0;
+		curr_entry = readdir(proc);
 	}
-
-	printf("root pid is %ld\n", (long)root_pid);
-	recursive_double_fork(s_root_height, "I am Root", 1, 31);
 }
