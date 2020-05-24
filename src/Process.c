ad69ca23ea92ca97df9cfca8f5538f7fefc88a75
Teddy Kishinevsky <kishtedy@gmail.com>
2020-05-24 09:26:34 -0400

    Finished first Context exercise

--- src/Process.c
+++ src/Process.c
@@ -13,64 +13,60 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <errno.h>
-#include <pthread.h>
+#include <setjmp.h>
 
 #define ERROR_VALUE (-1)
 
-pthread_mutex_t printing_mutex = PTHREAD_MUTEX_INITIALIZER;
+jmp_buf f1_state;
+jmp_buf f2_state;
 
-void error_exit_en(int en, const char* msg)
+void f1()
 {
-	errno = en;
+	printf("Entering first loop...\n");
+
+	while (1)
+	{
+		if (setjmp(f1_state) != 0)
+		{
+			printf("A");
+			longjmp(f2_state, 1);
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void f2()
+{
+	printf("Entering second loop...\n");
+
+	while (1)
+	{
+		if (setjmp(f2_state) != 0)
+		{
+			printf("B");
+			longjmp(f1_state, 1);
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void error_exit(const char* msg)
+{
 	perror(msg);
 	exit(ERROR_VALUE);
 }
 
-void perform_action(char letter)
-{
-	int i;
-
-	while (1)
-	{
-		pthread_mutex_lock(&printing_mutex);
-
-		for (i = 1; i <= 3; i++)
-		{
-			printf("%c", letter);
-		}
-
-		printf("\n");
-
-		fflush(stdout);
-
-		pthread_mutex_unlock(&printing_mutex);
-	}
-}
-
-void* thread_function(void* arg)
-{
-	char letter = *((char *) arg);
-	free(arg);
-
-	perform_action(letter);
-}
-
 int main(int argc, char* argv[])
 {
-	pthread_t tid;
-	char letter;
-	int err;
+	// Calling the functions to set their jump points, so that we can call them later
+	f1();
+	f2();
 
-	for (letter = 'A'; letter < 'Z'; letter++)
-	{
-		char* current_letter = (char *) malloc(sizeof(char));
-		*current_letter = letter;
-
-		if ((err = pthread_create(&tid, NULL, thread_function, (void *) current_letter)) != 0)
-		{
-			error_exit_en(err, "Thread creation failed");
-		}
-	}
-
-	perform_action(letter);
+	longjmp(f1_state, 1);
 }
