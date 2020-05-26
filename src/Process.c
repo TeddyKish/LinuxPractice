d3cfed12fe8ebdb84eb7b21fb5f539006ed01bbd
Teddy Kishinevsky <kishtedy@gmail.com>
2020-05-26 10:38:06 -0400

    Added the test program on which the fibers operate, still not complete

--- src/Process.c
+++ src/Process.c
@@ -9,10 +9,7 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <errno.h>
+#include "fibers9.h"
 
 void error_exit(const char* msg)
 {
@@ -20,7 +17,49 @@ void error_exit(const char* msg)
 	exit(-1);
 }
 
+void perform_action(char letter)
+{
+	while (1)
+	{
+		for (int i = 1; i <= 3; i++)
+		{
+			printf("%c", letter);
+		}
+
+		printf(" %d\n", fibers9_self());
+
+		fflush(stdout);
+
+		fibers9_yield();
+	}
+}
+
+void* thread_function(void* arg)
+{
+	char letter = *((char *) arg);
+	free(arg);
+
+	perform_action(letter);
+}
+
 int main(int argc, char* argv[])
 {
+	fibers9_init();
 
+	char letter;
+
+	for (letter = 'A'; letter < 'Z'; letter++)
+	{
+		char* current_letter = (char *) malloc(sizeof(char));
+		*current_letter = letter;
+
+		if (fibers9_create(thread_function, (void *) current_letter) == 0)
+		{
+			error_exit("Fiber creation failed");
+		}
+	}
+
+	perform_action(letter);
+
+	fibers9_destroy();
 }
