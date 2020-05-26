39efaa955ad01160fb1d68602ebf1b464552f819
Teddy Kishinevsky <kishtedy@gmail.com>
2020-05-26 04:49:19 -0400

    Added id functions and converted several parts to be generic

--- src/fibers9.c
+++ src/fibers9.c
@@ -19,21 +19,57 @@ typedef struct fiber_s
 fiber_t fibers[MAX_FIBERS];
 int active_fiber_id;
 
+static int get_free_id()
+{
+	int free_id = 1;
+	int is_free = 1;
+
+	while (is_free)
+	{
+		is_free = 0;
+
+		for (int i = 0; i < MAX_FIBERS; i++)
+		{
+			if (fibers[i].id == free_id)
+			{
+				is_free = 1;
+				free_id++;
+				break;
+			}
+		}
+	}
+
+	return free_id;
+}
+
+static fiber_t* get_fiber_by_id(int id)
+{
+	for (int i = 0; i < MAX_FIBERS; i++)
+	{
+		if (fibers[i].id == id)
+		{
+			return &(fibers[i]);
+		}
+	}
+
+	return NULL;
+}
+
 static fiber_t* get_lru_fiber()
 {
-	int lru_fiber_id = fibers[0].id;
+	fiber_t* lru_fiber = NULL;
 	time_t lru_fiber_time = fibers[0].last_usage;
 
 	for (int i = 1; i < MAX_FIBERS; i++)
 	{
 		if (fibers[i].last_usage < lru_fiber_time)
 		{
-			lru_fiber_id = fibers[i].id;
+			lru_fiber = &(fibers[i]);
 			lru_fiber_time = fibers[i].last_usage;
 		}
 	}
 
-	return lru_fiber_id;
+	return lru_fiber;
 }
 
 void fibers9_init()
@@ -62,10 +98,13 @@ int fibers9_create(fiber_main_t func_to_call, void* arg)
 
 void fibers9_yield()
 {
-	fibers[active_fiber_id].last_usage = time(NULL);
+	fiber_t* current_fiber = get_fiber_by_id(active_fiber_id); // Should never return NULL
+
+	current_fiber->last_usage = time(NULL);
+
 	fiber_t* lru_fiber = get_lru_fiber();
 
-	if (setjmp(fibers[active_fiber_id].context) == 0)
+	if (setjmp(current_fiber->context) == 0)
 	{
 		longjmp(lru_fiber->context, 0);
 	}
